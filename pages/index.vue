e364915b4f80859c3e5199f7f99f1c81d747a507
saleh1992 <salehsites@gmail.com>
2023-01-21 13:27:42 +0200

    add meta tage

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
- <div>
-  index page.
- </div>
+  <div>
+    index page.
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  head() {
+    return {
+      title: "welcome in dadjokes",
+      meta: [
+        {
+          hid: 'this is hid dadjokes',
+          name: "this is name of dadjokes",
+          description: " this is description",
+          dammy: "dammy"
+        }
+      ],
+    }
+  }
+
 }
 </script>
